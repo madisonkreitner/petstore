cb15c2a8fd79107cbede493b965fa36b52cce52b
Madison Kreitner <mkreitner@pfl.com>
2023-01-11 00:00:57 -0500

    Configure to use startup, nlog, seq

--- Petstore/Program.cs
+++ Petstore/Program.cs
@@ -1,25 +1,104 @@
-var builder = WebApplication.CreateBuilder(args);
+using NLog.Web;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace InternalOrderformService
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    /// <summary>
+    /// Main Program
+    /// </summary>
+    public static class Program
+    {
+        static private readonly string _appName = "Petstore";
+        static private readonly string _filenameFriendlyApp = string.Join('_', _appName.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries));
+        static private readonly string _environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+        static private readonly int _exposedPort = 64451;
+
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args">Commandline Argument</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //There is a bit of a chicken/egg issue with start-up and log. DI (with env, config settings)
+                //Using console for main start-up. NLog config is introduced when the Host builder completes with
+                //full logging
+                Console.WriteLine($"Starting {_appName}");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                //NLog: catch setup errors
+                Console.WriteLine("Stopped program because of following exception");
+                Console.WriteLine(e.ToString());
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Web Host Builder
+        /// </summary>
+        /// <param name="args">Commandline Arguments</param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(new[] { $"http://*:{_exposedPort}" });
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
+                    config.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: false);
+
+                    // Allows for environment specific configuration overrides.
+                    // Useful when deploying application into Kubernetes where
+                    // volumes can be configured to link Kubernetes ConfigMap objects 
+                    // into files in a local directory
+                    string filePath = $"/config/{_filenameFriendlyApp}-v1.json";
+                    Console.WriteLine($"Optional \"{filePath}\" config file exists? => {File.Exists(filePath)}");
+                    config.AddJsonFile(filePath, optional: true, reloadOnChange: false);
+
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+
+                    List<string> nlogConfigFiles = new()
+                    {
+                        // Set up log configuration precedence...
+                        $"/config/{_filenameFriendlyApp}-nlog.config",
+                        "/config/shared/nlog.config",
+                        string.IsNullOrWhiteSpace(_environment) ? "./nlog.Development.config" : $"./nlog.{_environment}.config",
+                        "./nlog.config"
+                    };
+                    Console.WriteLine($"Searching for nlog.config files in the following priority locations: [{string.Join(", ", nlogConfigFiles.Select(x => $"\"{x}\""))}]");
+                    string? nlogConfigFile = nlogConfigFiles.FirstOrDefault(f => File.Exists(f));
+                    if (string.IsNullOrEmpty(nlogConfigFile))
+                    {
+                        // Look for first config file found starting from the top of the drive
+                        string[] files = Directory.GetFiles("/", "nlog*.config", SearchOption.AllDirectories);
+                        if (files.Length == 0)
+                        {
+                            throw new FileNotFoundException($"Unable to find the file nlog.config anywhere on disk");
+                        }
+                        else
+                        {
+                            nlogConfigFile = files[0];
+                            Console.WriteLine($"Found an nlog.config file at the following location: {nlogConfigFile}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Found an nlog.config file at the following location: {nlogConfigFile}");
+                    }
+                    //full path needed NLog to find config in container
+                    logging.AddNLog(Path.GetFullPath(nlogConfigFile));
+                })
+              .UseNLog();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Petstore/Startup.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Text.Json.Serialization;
+
+namespace InternalOrderformService
+{
+    /// <summary>
+    /// Startup Class
+    /// </summary>
+    public class Startup
+    {
+        private readonly string _ServiceTitle = "Petstore";
+        private readonly string _ServiceDesc = "Manages pets";
+        private readonly WaitHandle _waitHandler = new ManualResetEvent(false);
+
+        /// <summary>
+        ///  Constructor
+        /// </summary>
+        /// <param name="configuration">Configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Configuration
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">Services Collection</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_waitHandler);
+
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                });
+            services.AddSwaggerDocument(o =>
+            {
+                o.Title = _ServiceTitle;
+                o.Description = _ServiceDesc;
+            });
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders =
+                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            });
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">Application Builder</param>
+        /// <param name="env">WebHostEnvironment</param>
+        /// <param name="logger">Logger</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use((context, next) =>
+            {
+                context.Request.EnableBuffering();
+                return next();
+            });
+
+            // UNCOMMENT to enable HTTPS redirection
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            logger.LogDebug("Finished running Startup::Configure()");
+
+            // WARNING: Following MUST be last statement in the function as this will release the background threads to begin processing
+            ((ManualResetEvent)_waitHandler).Set();
+        }
+    }
+}
